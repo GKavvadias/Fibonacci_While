f5c6a2e7bbeab2631a5bc8f3d720cf612c3155c5
unknown <80004643+GKavvadias@users.noreply.github.com>
2021-04-01 17:15:44 +0100

    Initial commit

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0, F0 = 0, F1 = 1, F;
+
+            while (n >= 0)
+            {
+                F = F0 + F1;
+                Console.WriteLine(F);
+                n++;
+                F0 = F1;
+                F1 = F;
+            }
+        }
+    }
+}
